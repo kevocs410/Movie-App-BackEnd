ad57da18fe5c3a20b7595a16cfdfbbe5474f6564
kevocs410 <kevocs410@yahoo.com>
2022-08-29 01:19:04 -0400

    set up backend

---
+++ models/shows.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const showSchema = new mongoose.Schema({
+    name: String,
+    genre: String,
+    image: String,
+    created: String
+},
+{ timestamps: true }
+);
+
+const Shows = mongoose.model('Show', showSchema)
+
+module.exports = Shows;
--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const express = require('express');
 const app = express ();
 const mongoose = require ('mongoose');
 const db = mongoose.connection;
-require('dotenv').config()
+require('dotenv').config();
+const Shows = require('./models/shows.js');
+const cors = require('cors');
+
+
+
+app.use(express.json());
+app.use(cors());
+
+
+
 //___________________
 //Port
 //___________________
@@ -25,7 +35,6 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // May or may not need these depending on your Mongoose version
 mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, }
 );
-
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
@@ -37,10 +46,33 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // Routes
 //___________________
 //localhost:3000
-app.get('/' , (req, res) => {
-  res.send('Hello World! , GOOD BYE');
-});
 
+app.post('/shows', (req, res) => {
+    Shows.create(req.body, (err, createdShow) =>{
+        res.json(createdShow)
+    })
+} )
+
+
+app.get('/shows', (req, res) => {
+    Shows.find({}, (err, foundShows) => {
+        res.json(foundShows);
+    })
+})
+
+app.delete('/shows/:id', (req,res) => {
+    Shows.findByIdAndRemove(req.params.id, (err,deletedShow) => {
+        res.json(deletedShow);
+    })
+})
+
+
+app.put('/shows/:id', (req,res) => {
+Shows.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedShow) =>{
+    res.json(updatedShow)
+})
+
+})
 //___________________
 //Listener
 //___________________
